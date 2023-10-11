e8de853a0c47fe99598559f0b3ab23e08c92449e
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2023-10-11 15:22:21 +0200

    Move instance variable to the top of the class

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -10,6 +10,7 @@ import java.util.Random;
 public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
+  private Random generator = new Random();
   private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
@@ -28,7 +29,6 @@ public class TorpedoStore {
     }
   }
 
-  Random generator = new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
